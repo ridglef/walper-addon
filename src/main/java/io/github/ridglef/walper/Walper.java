40feb999fbfae05ffff7b868a735c7c68b846280
ridglef <126202739+ridglef@users.noreply.github.com>
2023-05-10 09:26:45 -0400

    we walperin big time

--- src/main/java/io/github/ridglef/walper/Walper.java
+++ src/main/java/io/github/ridglef/walper/Walper.java
@@ -1,7 +1,7 @@
 package io.github.ridglef.walper;
 
 import io.github.ridglef.walper.hud.WalperHud;
-import io.github.ridglef.walper.modules.WalperSpam;
+import io.github.ridglef.walper.modules.*;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.hud.Hud;
@@ -16,7 +16,13 @@ public class Walper extends MeteorAddon {
     @Override
     public void onInitialize() {
         LOG.info("Walper Addon Started!");
-        Modules.get().add(new WalperSpam());
+        Modules m = Modules.get();
+        m.add(new WalperSpam());
+        m.add(new WalperHydration());
+        m.add(new WalperMode());
+        m.add(new  WalperTopHat());
+        m.add(new WalpuhMovin());
+        m.add(new WalpuhThighHighlighter());
         Hud.get().register(WalperHud.INFO);
     }
 

ad72f2a5d3394c24ff05a93a4123ef8d5cf17fa0
ridglef <126202739+ridglef@users.noreply.github.com>
2023-06-17 10:55:23 -0400

    1.20

--- src/main/java/io/github/ridglef/walper/mixins/WalperSplash.java
+++ src/main/java/io/github/ridglef/walper/mixins/WalperSplash.java
@@ -1,6 +1,7 @@
 package io.github.ridglef.walper.mixins;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.SplashTextRenderer;
 import net.minecraft.client.resource.SplashTextResourceSupplier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(SplashTextResourceSupplier.class)
 public class WalperSplash {
     @Inject(method = "get", at = @At("HEAD"), cancellable = true)
-    private void walper(CallbackInfoReturnable<String> cir) {
-        cir.setReturnValue("Walper Owns Me And All");
+    private void walper(CallbackInfoReturnable<SplashTextRenderer> cir) {
+        cir.setReturnValue(new SplashTextRenderer("Walper Owns Me And All"));
     }
 }
